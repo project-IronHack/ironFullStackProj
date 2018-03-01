08be4cc867e63915b4c5e801a8207253d3322584
Debby ermel <debbyermel@gmail.com>
2018-03-01 17:30:09 -0600

    form review

--- public/javascripts/js.js
+++ public/javascripts/js.js
@@ -7,5 +7,4 @@ $(document).ready(function ($) {
         strengthButtonClass: 'button_strength',
         strengthButtonTextToggle: 'Hide Password'
     });
-
 });
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router  = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index');
+  res.render('payment'); //REMEMBER TO MODIFY THIS ROUTE...JUST USING FOR TEST
 });
 
 module.exports = router;
